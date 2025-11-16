9c173af7c387ccb6098f61cbba584a7077ca27eb
Ankit Kumar <ankit233kumar@gmail.com>
2025-11-16 09:27:38 +0530

    initBinder inclusion

--- SampleForm/src/main/java/com/selenium/Controllers/MyController.java
+++ SampleForm/src/main/java/com/selenium/Controllers/MyController.java
@@ -1,8 +1,11 @@
 package com.selenium.Controllers;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,20 +15,29 @@ import jakarta.validation.Valid;
 
 @Controller
 public class MyController {
-	
+
 	@RequestMapping("/")
 	public String formFill(Model model) {
 		model.addAttribute("UserInfo", new UserInfoDTO());
 		return "Home-page";
 	}
-	
+
 	@RequestMapping("/register")
-	public String resultDisplay(@Valid @ModelAttribute("UserInfo")UserInfoDTO user, BindingResult result) {
-		if(result.hasErrors()) {
+	public String resultDisplay(@Valid @ModelAttribute("UserInfo") UserInfoDTO user, BindingResult result) {
+		System.out.println(user.getApplicantname());
+		if (result.hasErrors()) {
 			return "Home-page";
-		}else {
+		} else {
 			return "form-display";
 		}
-		
 	}
+
+	@InitBinder
+	public void initBinder(WebDataBinder webDataBinder) {
+		System.out.println("Hello");
+		StringTrimmerEditor editor = new StringTrimmerEditor(true);
+		webDataBinder.registerCustomEditor(String.class, "Applicantname", editor);
+
+	}
+
 }
--- SampleForm/src/main/java/com/selenium/Model/UserInfoDTO.java
+++ SampleForm/src/main/java/com/selenium/Model/UserInfoDTO.java
@@ -1,44 +1,47 @@
 package com.selenium.Model;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 public class UserInfoDTO {
-	
-	@NotBlank(message = "Name should not be blank")
+
+	@NotEmpty(message = "Name should not be blank")
 	@Size(min = 5, max = 20, message = "Your name should have character between 5 - 15")
 	private String Applicantname;
 	@Valid
 	private CommunicationDTO communicationDTO;
-	@Age(lower= 45, upper = 50)
+	@Age(lower = 45, upper = 50)
 	private int Applicantage;
+
 	public String getApplicantname() {
 		return Applicantname;
 	}
+
 	public void setApplicantname(String applicantname) {
 		Applicantname = applicantname;
 	}
-	
-	
+
 	public CommunicationDTO getCommunicationDTO() {
 		return communicationDTO;
 	}
+
 	public void setCommunicationDTO(CommunicationDTO communicationDTO) {
 		this.communicationDTO = communicationDTO;
 	}
+
 	public int getApplicantage() {
 		return Applicantage;
 	}
+
 	public void setApplicantage(int applicantage) {
 		Applicantage = applicantage;
 	}
+
 	@Override
 	public String toString() {
 		return "UserInfoDTO [Applicantname=" + Applicantname + ", communicationDTO=" + communicationDTO
 				+ ", Applicantage=" + Applicantage + "]";
 	}
-	
-	
 
 }
